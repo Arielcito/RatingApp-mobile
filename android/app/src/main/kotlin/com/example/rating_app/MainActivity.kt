abd396c92aeb4781b9e148ddced971f02a7fa46b
Dante <dantezulli2004@gmail.com>
2024-10-21 21:11:48 -0300

    elimino las cosas viejas. stack superior de welcome screen completo

--- android/app/src/main/kotlin/com/example/rating_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.rating_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- ratingapp_mobile/lib/main.dart
+++ ratingapp_mobile/lib/main.dart
@@ -1,21 +1,19 @@
 import 'package:flutter/material.dart';
- 
+import 'package:ratingapp_mobile/screens/welcome_screen.dart';
+
 void main() => runApp(MyApp());
- 
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      title: 'Rating App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Material App Bar'),
+          toolbarHeight: 0.0,
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
+        body: const WelcomeScreen(),
       ),
     );
   }
