9b6ef5daf40e9b49796f68b9f41cfa3d24d93c05
Dante <dantezulli2004@gmail.com>
2024-10-29 21:11:08 -0300

    implementa controles y progressbar con fullscreen en el videoplayer

--- ratingapp_mobile/lib/screens/widgets/video_player_widget.dart
+++ ratingapp_mobile/lib/screens/widgets/video_player_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:video_player/video_player.dart';
 
 class VideoPlayerWidget extends StatefulWidget {
@@ -19,20 +18,143 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
       'assets/videos/sample-5s.mp4',
     )..initialize().then((_) {
         _controller.play();
+        _controller.setLooping(true);
         setState(() {});
       });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _enterFullScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => FullScreenVideoPlayer(controller: _controller),
+      ),
+    );
+  }
+
+  void _rewind() {
+    final newPosition = _controller.value.position - Duration(seconds: 5);
+    _controller
+        .seekTo(newPosition >= Duration.zero ? newPosition : Duration.zero);
+  }
+
+  void _fastForward() {
+    final maxPosition = _controller.value.duration;
+    final newPosition = _controller.value.position + Duration(seconds: 5);
+    _controller.seekTo(newPosition <= maxPosition ? newPosition : maxPosition);
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Center(
+            child: AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              child: _controller.value.isInitialized
+                  ? VideoPlayer(_controller)
+                  : Container(),
+            ),
+          ),
+          if (_controller.value.isInitialized)
+            Positioned(
+              bottom: 40,
+              left: 0,
+              right: 0,
+              child: Row(
+                children: [
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: VideoProgressIndicator(
+                      _controller,
+                      allowScrubbing: true,
+                      colors: const VideoProgressColors(
+                        playedColor: Colors.red,
+                        bufferedColor: Colors.grey,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                  ),
+                  IconButton(
+                    icon: const Icon(Icons.fullscreen, color: Colors.red),
+                    onPressed: _enterFullScreen,
+                  ),
+                ],
+              ),
+            ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon:
+                      const Icon(Icons.replay_5, color: Colors.white, size: 24),
+                  onPressed: _rewind,
+                ),
+                const SizedBox(width: 25),
+                IconButton(
+                  icon: Icon(
+                    _controller.value.isPlaying
+                        ? Icons.pause
+                        : Icons.play_arrow,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _controller.value.isPlaying
+                          ? _controller.pause()
+                          : _controller.play();
+                    });
+                  },
+                ),
+                const SizedBox(width: 25),
+                IconButton(
+                  icon: const Icon(Icons.forward_5,
+                      color: Colors.white, size: 24),
+                  onPressed: _fastForward,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FullScreenVideoPlayer extends StatelessWidget {
+  final VideoPlayerController controller;
+
+  const FullScreenVideoPlayer({Key? key, required this.controller})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Center(
         child: AspectRatio(
-          aspectRatio: _controller.value.aspectRatio,
-          child: _controller.value.isInitialized
-              ? VideoPlayer(_controller)
-              : Container(),
+          aspectRatio: controller.value.aspectRatio,
+          child: VideoPlayer(controller),
         ),
       ),
     );
